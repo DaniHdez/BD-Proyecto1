0197d00d0c76655140d68dba81210564e627532c
Alejandra Castrillo <ale.castrillom97@gmail.com>
2019-09-29 01:08:03 -0600

    Updated scripts

---
+++ scripts/populateDB.sql
@@ -0,0 +1,60 @@
+/* POPULATE DATABASE */
+
+USE FarmaTEC
+GO
+
+/* POBLANDO Provincia */
+EXECUTE sp_push_Provincia 'San José';
+EXECUTE sp_push_Provincia 'Heredia';
+EXECUTE sp_push_Provincia 'Cartago';
+EXECUTE sp_push_Provincia 'Alajuela';
+EXECUTE sp_push_Provincia 'Puntarenas';
+EXECUTE sp_push_Provincia 'Guanacaste';
+EXECUTE sp_push_Provincia 'Limón';
+/* POBLANDO Marca*/
+EXECUTE sp_push_Marca 'Adenuric';
+EXECUTE sp_push_Marca 'Balzak';
+EXECUTE sp_push_Marca 'Disgren';
+EXECUTE sp_push_Marca 'Enantyum';
+EXECUTE sp_push_Marca 'GSK';
+EXECUTE sp_push_Marca 'Pfizer';
+EXECUTE sp_push_Marca 'Panadol';
+/* POBLANDO Farmacia */
+EXECUTE sp_push_Farmacia 'La Bomba',384612874,'9.932004','-84.051840',223465563,'labomba@servicioalcliente.com','Lunes a Viernes de 9:00am a 9:00pm',₡0.00,'Heredia';
+EXECUTE sp_push_Farmacia 'La Arboleda',378659898,'9.859968','-83.922104',23239090,'laarboleda@servicioalcliente.com','Lunes a Domingo de 6:00am a 10:00pm',₡0.00,'Cartago';
+EXECUTE sp_push_Farmacia 'La Fischel',377768778,'9.979405','-84.089527',22546754,'lafischel@servicioalcliente.com','Lunes a Sábado de 7:00am a 8:00pm',₡0.00,'San José';
+/* POBLANDO Medicamento */
+DECLARE @Img1 VARBINARY(MAX);
+DECLARE @Img2 VARBINARY(MAX);
+DECLARE @Img3 VARBINARY(MAX);
+SELECT * FROM OPENROWSET(BULK N'C:\Users\alecastrillo\Pictures\enantyum.jpg', SINGLE_BLOB) AS Img1;
+SET @Img1=(SELECT * FROM OPENROWSET(BULK N'C:\Users\alecastrillo\Pictures\enantyum.jpg', SINGLE_BLOB) AS Img1);
+SET @Img2=(SELECT * FROM OPENROWSET(BULK N'C:\Users\alecastrillo\Pictures\paracetamol.jpeg', SINGLE_BLOB) as Img2);
+SET @Img3=(SELECT * FROM OPENROWSET(BULK N'C:\Users\alecastrillo\Pictures\viagra.jpg', SINGLE_BLOB) as Img3)
+SELECT * FROM Farmacia
+EXECUTE sp_push_Medicamento 'Enantyum Gel','MED20190001','Analgésico perteneciente al grupo de medicamentos denominados antiinflamatorios no esteroideos (AINE).','0','1 cada 8h','Náuseas, vómitos, diarrea y dolor o ardor de estómago.',@Img1,₡1500,'Analgésico','Enantyum';
+EXECUTE sp_push_Medicamento 'Paracetamol','MED20190010','Fármaco con propiedades analgésicas y antipiréticas utilizado principalmente para tratar la fiebre, y el dolor leve y moderado.','10 a 15 mg (3 a 5 gotas) por kilo de peso','1 o medio comprimido (1 g-500 mg) cada mínimo 4h.','Se pueden manifestar con síntomas como fatiga inusual, anorexia, náuseas y/o vómitos, dolor abdominal, ictericia, orina oscura o deposiciones blanquecinas.',@Img2,₡200,'Analgésico y antipirético','Panadol';
+EXECUTE sp_push_Medicamento 'Viagra','MED20190101','Sustancia usada para tratar la disfunción eréctil.','0','1 cada 8h','Dolor de cabeza, enrojecimiento, dispepsia, trastornos visuales, como distorsión de la visión de los colores y visión borrosa.',@Img3,₡3000,'Inhibidores','Pfizer';
+/* POBLANDO Empleado */
+EXECUTE sp_push_Empleado 'Juan','Castillo','Rojas',1,1,'juancastillorojas@gmail.com','contrasenadejuan',384612874;
+EXECUTE sp_push_Empleado 'María','Gómez','Castro',2,1,'mariagomezcastro@gmail.com','contrasenademaria',378659898;
+EXECUTE sp_push_Empleado 'Alberto','Rodriguez','Torres',3,1,'albertorodrigueztorres@gmail.com','contrasenadealberto',377768778;
+/* POBLANDO Cliente*/
+EXECUTE sp_push_Cliente 116892238, 'Ricardo','Jiménez','Muñoz','88776655',1,'San José'
+EXECUTE sp_push_Cliente 109988787, 'Carla','Camacho','Hidalgo','68554334',2,'San José'
+EXECUTE sp_push_Cliente 345547890, 'Manuel','Jiménez','Valvere','89898989',3,'Cartago'
+EXECUTE sp_push_Cliente 398677655, 'Jimena','Mora','Fernández','67676767',1,'Cartago'
+EXECUTE sp_push_Cliente 207748833, 'Daniela','Masis','Céspedes','64545454',2,'Heredia'
+EXECUTE sp_push_Cliente 223443458, 'Emily','Arredondo','Nuñez','89786756',3,'Heredia'
+/* POBLANDO FULL Pedido */
+EXECUTE sp_push_FullPedido '01/01/19 22:23:33.666',1,₡5500,'PED20180001',1,116892238,384612874,'"MED20190001","MED20190010","MED20190101"','1,5,1'
+EXECUTE sp_push_FullPedido '07/01/19 22:23:33.666',1,₡1200,'PED20180001',1,116892238,377768778,'MED20190010','6'
+EXECUTE sp_push_FullPedido '01/01/19 22:23:33.666',1,₡400,'PED20180001',1,109988787,378659898,'MED20190010','2'
+EXECUTE sp_push_FullPedido '02/01/19 22:23:33.666',1,₡1200,'PED20180001',1,109988787,384612874,'MED20190010','1'
+EXECUTE sp_push_FullPedido '03/01/19 22:23:33.666',1,₡4500,'PED20180001',1,116892238,378659898,'MED20190101','3'
+EXECUTE sp_push_FullPedido '01/02/19 22:23:33.666',1,₡5500,'PED20180001',1,116892238,384612874,'MED20190001,MED20190010,MED20190101','1,5,1'
+EXECUTE sp_push_FullPedido '07/02/19 22:23:33.666',1,₡1200,'PED20180001',1,116892238,378659898,'MED20190010','6'
+EXECUTE sp_push_FullPedido '01/02/19 22:23:33.666',1,₡400,'PED20180001',1,109988787,378659898,'MED20190010','2'
+EXECUTE sp_push_FullPedido '02/02/19 22:23:33.666',1,₡1200,'PED20180001',1,109988787,377768778,'MED20190010','1'
+EXECUTE sp_push_FullPedido '03/02/19 22:23:33.666',1,₡4500,'PED20180001',1,116892238,384612874,'MED20190101','3'
+
--- scripts/sps.sql
+++ scripts/sps.sql
@@ -27,15 +27,22 @@ GO
 CREATE PROCEDURE sp_push_Farmacia
 	@Nombre VARCHAR(MAX),
 	@CedJuridica BIGINT,
-	@Ubicacion VARCHAR(MAX),
+	@PuntoA VARCHAR(MAX),
+	@PuntoB VARCHAR(MAX),
 	@Telefono BIGINT,
 	@Correo VARCHAR(MAX),
 	@Horario VARCHAR(MAX),
-	@TotalRecaudado MONEY
+	@TotalRecaudado MONEY,
+	@Provincia VARCHAR(MAX)
 AS
 BEGIN
-	INSERT INTO Farmacia (Nombre,CedJuridica,Ubicacion,Telefono,Correo,Horario,TotalRecaudado)  
-	VALUES (@Nombre,@CedJuridica,@Ubicacion,@Telefono,@Correo,@Horario,@TotalRecaudado)
+	DECLARE @Ubicacion geography;
+	SET @Ubicacion = geography::Point(@PuntoA, @PuntoB, '4326');
+	DECLARE @IdProvincia INT;
+	SET @IdProvincia=0;
+	SELECT @IdProvincia=Id FROM Provincia WHERE Nombre=@Provincia;
+	INSERT INTO Farmacia (Nombre,CedJuridica,Ubicacion,Telefono,Correo,Horario,TotalRecaudado,IdProvincia)  
+	VALUES (@Nombre,@CedJuridica,@Ubicacion,@Telefono,@Correo,@Horario,@TotalRecaudado,@IdProvincia)
 END
 GO
 /* Empleado */
@@ -46,29 +53,20 @@ CREATE PROCEDURE sp_push_Empleado
 	@Tipo INT,
 	@Estado BIT,
 	@Correo VARCHAR(MAX),
-	@Contraseña VARCHAR(MAX)
+	@Contraseña VARCHAR(MAX),
+	@CedJurídica BIGINT
 AS
 BEGIN
-	INSERT INTO Empleado(Nombre,Apellido1,Apellido2,Tipo,Estado,Correo,Contraseña)  
-	VALUES (@Nombre,@Apellido1,@Apellido2,@Tipo,@Estado,@Correo,@Contraseña)
+	DECLARE @IdFarmacia INT;
+	SET @IdFarmacia=0;
+	SELECT @IdFarmacia=Id FROM Farmacia WHERE CedJuridica=@CedJurídica;
+	PRINT (@IdFarmacia);
+	INSERT INTO Empleado(Nombre,Apellido1,Apellido2,Tipo,Estado,Correo,Contraseña,IdFarmacia)  
+	VALUES (@Nombre,@Apellido1,@Apellido2,@Tipo,@Estado,@Correo,@Contraseña,@IdFarmacia)
 END
 GO
 /* Cliente */
 CREATE PROCEDURE sp_push_Cliente
-	@Cedula BIGINT,
-	@Nombre VARCHAR(MAX),
-	@Apellido1 VARCHAR(MAX),
-	@Apellido2 VARCHAR(MAX),
-	@Telefono BIGINT,
-	@Tipo INT,
-	@IdProvincia INT
-AS
-BEGIN
-	INSERT INTO Cliente(Cedula,Nombre,Apellido1,Apellido2,Telefono,Tipo,IdProvincia)
-	VALUES (@Cedula,@Nombre,@Apellido1,@Apellido2,@Telefono,@Tipo,@IdProvincia)
-END
-GO
-CREATE PROCEDURE sp_push_Cliente_sin_fk
 	@Cedula BIGINT,
 	@Nombre VARCHAR(MAX),
 	@Apellido1 VARCHAR(MAX),
@@ -88,37 +86,22 @@ GO
 /* Medicamento */
 CREATE PROCEDURE sp_push_Medicamento
 	@Nombre VARCHAR(MAX),
+	@CodigoDeMedicamento VARCHAR(MAX),
 	@Descripcion VARCHAR(MAX),
 	@DosisNinos VARCHAR(MAX),
 	@DosisAdultos VARCHAR(MAX),
 	@EfectosSecundarios VARCHAR(MAX),
-	@Foto IMAGE,
+	@Foto VARBINARY(MAX),
 	@Precio MONEY,
-	@Tipo INT, 
-	@IdMarca INT
-AS
-BEGIN
-	INSERT INTO Medicamento(Nombre,Descripcion,DosisNinos,DosisAdultos,EfectosSecundarios,Foto,Precio,Tipo,IdMarca)
-	VALUES (@Nombre,@Descripcion,@DosisNinos,@DosisAdultos,@EfectosSecundarios,@Foto,@Precio,@Tipo,@IdMarca)
-END
-GO
-CREATE PROCEDURE sp_push_Medicamento_sin_fk
-	@Nombre VARCHAR(MAX),
-	@Descripcion VARCHAR(MAX),
-	@DosisNinos VARCHAR(MAX),
-	@DosisAdultos VARCHAR(MAX),
-	@EfectosSecundarios VARCHAR(MAX),
-	@Foto IMAGE,
-	@Precio MONEY,
-	@Tipo INT, 
+	@Tipo VARCHAR(MAX), 
 	@Marca VARCHAR(MAX)
 AS
 BEGIN
 	DECLARE @IdMarca INT;
 	SET @IdMarca = 1;
 	SELECT @IdMarca=Id FROM Marca WHERE Nombre=@Marca;
-	INSERT INTO Medicamento(Nombre,Descripcion,DosisNinos,DosisAdultos,EfectosSecundarios,Foto,Precio,Tipo,IdMarca)
-	VALUES (@Nombre,@Descripcion,@DosisNinos,@DosisAdultos,@EfectosSecundarios,@Foto,@Precio,@Tipo,@IdMarca)
+	INSERT INTO Medicamento(Nombre,CodigoDeMedicamento,Descripcion,DosisNinos,DosisAdultos,EfectosSecundarios,Foto,Precio,Tipo,IdMarca)
+	VALUES (@Nombre,@CodigoDeMedicamento,@Descripcion,@DosisNinos,@DosisAdultos,@EfectosSecundarios,@Foto,@Precio,@Tipo,@IdMarca)
 END
 GO
 /* FarmaciaXMedicamento */
@@ -133,17 +116,17 @@ BEGIN
 END
 GO
 CREATE PROCEDURE sp_push_FarmaciaXMedicamento_sin_fk
-	@Farmacia INT,
-	@Medicamento INT,
+	@CedJuridica BIGINT,
+	@CodigoDeMedicamento VARCHAR(MAX),
 	@Stock INT
 AS
 BEGIN
 	DECLARE @IdFarmacia INT;
 	SET @IdFarmacia = 0;
-	SELECT @IdFarmacia=Id FROM Farmacia WHERE Nombre=@Farmacia;
+	SELECT @IdFarmacia=Id FROM Farmacia WHERE CedJuridica=@CedJuridica
 	DECLARE @IdMedicamento INT;
 	SET @IdMedicamento = 0;
-	SELECT @IdMedicamento=Id FROM Medicamento WHERE Nombre=@Medicamento;
+	SELECT @IdMedicamento=Id FROM Medicamento WHERE CodigoDeMedicamento=@CodigoDeMedicamento
 	INSERT INTO FarmaciaXMedicamento(IdFarmacia,IdMedicamento,Stock)
 	VALUES (@IdFarmacia,@IdMedicamento,@Stock)
 END
@@ -154,81 +137,107 @@ CREATE PROCEDURE sp_push_Pedido
 	@Estado INT,
 	@Monto MONEY,
 	@Tipo INT,
-	@IdCliente INT,
-	@IdFarmacia INT
-AS
-BEGIN
-	INSERT INTO Pedido(Fecha,Estado,Monto,Tipo,IdCliente,IdFarmacia)
-	VALUES (@Fecha,@Estado,@Monto,@Tipo,@IdCliente,@IdFarmacia)
-END
-GO
-CREATE PROCEDURE sp_push_Pedido_sin_fk
-	@Fecha DATETIME,
-	@Estado INT,
-	@Monto MONEY,
-	@Tipo INT,
 	@CedulaCliente BIGINT,
-	@Farmacia INT
+	@CedJuridica BIGINT,
+	@CodigoPedido VARCHAR(MAX)
 AS
 BEGIN
 	DECLARE @IdFarmacia INT;
 	SET @IdFarmacia = 0;
-	SELECT @IdFarmacia=Id FROM Farmacia WHERE Nombre=@Farmacia;
+	SELECT @IdFarmacia=Id FROM Farmacia WHERE CedJuridica=@CedJuridica;
 	DECLARE @IdCliente INT;
 	SET @IdCliente = 0;
-	SELECT @IdCliente=Id FROM Cliente WHERE Nombre=@CedulaCliente;
-	INSERT INTO Pedido(Fecha,Estado,Monto,Tipo,IdCliente,IdFarmacia)
-	VALUES (@Fecha,@Estado,@Monto,@Tipo,@IdCliente,@IdFarmacia)
+	SELECT @IdCliente=Id FROM Cliente WHERE Cedula=@CedulaCliente;
+	INSERT INTO Pedido(Fecha,Estado,Monto,CodigoPedido,Tipo,IdCliente,IdFarmacia)
+	VALUES (@Fecha,@Estado,@Monto,@CodigoPedido,@Tipo,@IdCliente,@IdFarmacia);
 END
 GO
 /* PedidoXMedicamento*/
 CREATE PROCEDURE sp_push_PedidoXMedicamento
 	@Cantidad INT,
-	@IdPedido INT,
-	@IdMedicamento INT
+	@CodigoPedido VARCHAR(MAX),
+	@CodigoDeMedicamento VARCHAR(MAX),
+	@CedJuridicaFarmacia BIGINT
 AS
 BEGIN
-	INSERT INTO PedidoXMedicamento(Cantidad,IdPedido,IdMedicamento)
-	VALUES (@Cantidad, @IdPedido,@IdMedicamento)
+	DECLARE @Stock INT;
+	SET @Stock = 0;
+	SELECT @Stock=fxm.Stock FROM FarmaciaXMedicamento fxm INNER JOIN Farmacia f ON f.CedJuridica=@CedJuridicaFarmacia WHERE fxm.IdFarmacia=f.Id;
+	IF @Stock>=@Cantidad
+	BEGIN
+		DECLARE @IdMedicamento INT;
+		SET @IdMedicamento = 0;
+		SELECT @IdMedicamento=Id FROM Medicamento WHERE CodigoDeMedicamento=@CodigoDeMedicamento;
+		DECLARE @IdPedido INT;
+		SET @IdPedido = 0;
+		SELECT @IdPedido=Id FROM Pedido WHERE CodigoPedido=@CodigoPedido;
+		INSERT INTO PedidoXMedicamento(Cantidad,IdPedido,IdMedicamento)
+			VALUES (@Cantidad, @IdPedido,@IdMedicamento);
+		UPDATE FarmaciaXMedicamento 
+			SET Stock=Stock-@Cantidad 
+			FROM Farmacia f INNER JOIN FarmaciaXMedicamento fxm ON f.Id=fxm.IdFarmacia 
+			WHERE f.CedJuridica=@CedJuridicaFarmacia AND IdMedicamento=@IdMedicamento
+	END
 END
 GO
-CREATE PROCEDURE sp_push_PedidoXMedicamento_sin_fk
-	@Cantidad INT,
-	@IdPedido INT,
-	@Medicamento INT
-AS
-BEGIN
-	DECLARE @IdMedicamento INT;
-	SET @IdMedicamento = 0;
-	SELECT @IdMedicamento=Id FROM Medicamento WHERE Nombre=@Medicamento;
-	INSERT INTO PedidoXMedicamento(Cantidad,IdPedido,IdMedicamento)
-	VALUES (@Cantidad, @IdPedido,@IdMedicamento)
-END
+/* FULL Pedido */
+CREATE TYPE ListaDeCodigosDeMedicamentos AS TABLE (Idx INT, CodigoDeMedicamento VARCHAR(MAX))
+GO
+CREATE TYPE ListaDeCantidades AS TABLE (Idx INT, CantidadDeMedicamento INT)
 GO
 
+CREATE PROCEDURE sp_push_FullPedido
+	@Fecha DATETIME,
+	@Estado INT,
+	@Monto MONEY,
+	@CodigoPedido VARCHAR(MAX),
+	@Tipo INT,
+	@CedulaCliente BIGINT,
+	@CedJuridica BIGINT,
+	@StringCodigoDeMedicamentos VARCHAR(MAX),
+	@StringCantidades VARCHAR(MAX)
+AS
+BEGIN
+
+	DECLARE @ListaCodigoDeMedicamentos ListaDeCodigosDeMedicamentos;
+	DECLARE @ListaDeCantidades ListaDeCantidades;
+	
+	DECLARE @QueryDeListas VARCHAR(MAX);
+	SET @QueryDeListas = 'INSERT INTO @ListaCodigoDeMedicamentos VALUES(' + @StringCodigoDeMedicamentos + ')';
+	PRINT(@QueryDeListas);
+	EXECUTE(@QueryDeListas);
+	
+	SET @QueryDeListas = 'INSERT INTO @ListaDeCantidades VALUES(' + @StringCantidades + ')';
+	PRINT(@QueryDeListas);
+	EXECUTE(@QueryDeListas);
+
+	EXECUTE sp_push_Pedido @Fecha,@Estado,@Monto,@Tipo,@CedulaCliente,@CedJuridica,@CodigoPedido;
+
+	DECLARE @i INT;
+	SET @i=1;
+
+	DECLARE @Cantidad INT;
+	DECLARE @CodigoDeMedicamento VARCHAR(MAX);
+
+	WHILE (@i <= (SELECT MAX(Idx) FROM @ListaCodigoDeMedicamentos))
+	BEGIN
+		SELECT @Cantidad=CantidadDeMedicamento FROM @ListaDeCantidades WHERE Idx=@i;
+		SELECT @CodigoDeMedicamento=CodigoDeMedicamento FROM @ListaCodigoDeMedicamentos WHERE Idx=@i;
+		EXECUTE sp_push_PedidoXMedicamento @Cantidad,@CodigoPedido,@CodigoDeMedicamento,@CedJuridica;
+	END
+END
+GO
 /**************** GET *****************/
 
-USE FarmaTEC
-GO
 /* Cantidad de dinero recaudado en sucursal */
 CREATE PROCEDURE sp_get_DineroRecaudadoEnSucursal
-	@Id INT
+	@CedJuridica BIGINT
 AS
 BEGIN
-	SELECT TotalRecaudado FROM Farmacia WHERE Id = @Id
+	SELECT TotalRecaudado FROM Farmacia WHERE CedJuridica=@CedJuridica
 END
 GO
-CREATE PROCEDURE sp_get_DineroRecaudadoEnSucursal_sin_fk
-	@Farmacia INT
-AS
-BEGIN
-	DECLARE @IdFarmacia INT;
-	SET @IdFarmacia = 0;
-	SELECT @IdFarmacia=Id FROM Medicamento WHERE Nombre=@Farmacia;
-	SELECT TotalRecaudado FROM Farmacia WHERE Id = @IdFarmacia
-END
-GO
-/* Pedidos en un rango de fechas*/
+/* Pedidos en un rango de fechas */
 CREATE PROCEDURE sp_get_PedidosEnRango
 	@FechaInicial DATETIME,
 	@FechaFinal DATETIME
@@ -237,7 +246,7 @@ BEGIN
 	SELECT Fecha,Estado,Monto,Tipo,IdCliente,IdFarmacia FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial
 END
 GO
-/* Cantida de pedidos en un rango de fechas*/
+/* Cantida de pedidos en un rango de fechas */
 CREATE PROCEDURE sp_get_CantidadDePedidosEnRango
 	@FechaInicial DATETIME,
 	@FechaFinal DATETIME
@@ -246,29 +255,30 @@ BEGIN
 	SELECT COUNT(Id) AS CantidadDePedidos FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial
 END
 GO
-/* Pedidos por cliente en un rango de fechas*/
+/* Pedidos por cliente en un rango de fechas */
 CREATE PROCEDURE sp_get_PedidosXClienteEnRango
 	@FechaInicial DATETIME,
 	@FechaFinal DATETIME,
 	@CedulaCliente INT
 AS
 BEGIN
-	DECLARE @IdCliente INT;
-	SET @IdCliente = 0;
-	SELECT @IdCliente=Id FROM Cliente WHERE Cedula=@CedulaCliente;
-	SELECT Fecha,Estado,Monto,Tipo,IdCliente,IdFarmacia FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial AND IdCliente=@IdCliente
+	SELECT p.CodigoPedido,p.Fecha,p.Estado,p.Monto,p.Tipo,c.Nombre,c.Apellido1,c.Apellido2,c.Cedula,f.Nombre
+		FROM Pedido p 
+		INNER JOIN Cliente c ON p.IdCliente=c.Id
+		INNER JOIN Farmacia f ON p.IdFarmacia=f.Id
+		WHERE P.Fecha<=@FechaFinal AND p.Fecha>=@FechaInicial AND c.Cedula=@CedulaCliente
 END
 GO
-/* Monto promedio pagado por clientes en rango  */
+/* Monto promedio pagado por todos los clientes en rango */
 CREATE PROCEDURE sp_get_MontoPromedioXClientesEnRango
 	@FechaInicial DATETIME,
 	@FechaFinal DATETIME
 AS
 BEGIN
-	SELECT AVG(Monto) AS Promedio FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial
+	SELECT AVG(Monto) AS Promedio FROM Pedido WHERE Fecha<=@FechaFinal AND Fecha>=@FechaInicial
 END
 GO
-/* Monto promedio pagado por cada cliente en rango  */
+/* Monto promedio pagado por cada cliente en rango */
 CREATE PROCEDURE sp_get_MontoPromedioXCadaClienteEnRango
 	@FechaInicial DATETIME,
 	@FechaFinal DATETIME
@@ -276,7 +286,7 @@ AS
 BEGIN
 	DECLARE @PromedioXCliente TABLE(Promedio FLOAT, IdCliente INT);
 	INSERT INTO @PromedioXCliente
-	SELECT AVG(Monto), IdCliente FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial GROUP BY IdCliente WITH ROLLUP
+		SELECT AVG(Monto), IdCliente FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial GROUP BY IdCliente WITH ROLLUP
 	SELECT c.Nombre, c.Apellido1, c.Apellido2,pxc.Promedio AS InfoCliente FROM Cliente c INNER JOIN @PromedioXCliente pxc ON pxc.IdCliente=c.Id
 END
 GO
@@ -299,7 +309,7 @@ BEGIN
 	SELECT SUM(Monto) FROM Pedido WHERE IdFarmacia=@IdFarmacia AND Tipo>@Tipo
 END
 GO
-/* Top Mejores Clientes */
+/* Top 3 Mejores Clientes */
 CREATE PROCEDURE sp_get_TopClientes
 	@FechaInicial DATETIME,
 	@FechaFinal DATETIME
@@ -308,13 +318,15 @@ BEGIN
 	DECLARE @PromedioXCliente TABLE(Promedio FLOAT, IdCliente INT);
 	INSERT INTO @PromedioXCliente
 	SELECT AVG(Monto), IdCliente FROM Pedido WHERE Fecha<@FechaFinal AND Fecha>@FechaInicial GROUP BY IdCliente WITH ROLLUP
-	SELECT TOP 3 c.Nombre, c.Apellido1, c.Apellido2,pxc.Promedio AS InfoCliente 
+	SELECT TOP 3 c.Nombre, c.Apellido1, c.Apellido2,c.Cedula,pxc.Promedio AS InfoCliente 
 	FROM Cliente c INNER JOIN @PromedioXCliente pxc 
 	ON pxc.IdCliente=c.Id 
 	ORDER BY Promedio
 END
 GO
-/* Get info Cliente */
+
+/** Generales **/
+/* Get info Cliente por cédula */
 CREATE PROCEDURE sp_get_Cliente
 	@CedulaCliente BIGINT
 AS
@@ -325,22 +337,128 @@ BEGIN
 	WHERE Cedula=@CedulaCliente
 END
 GO
-
+/* Obtener info de farmacia por CedJuridica */
+CREATE PROCEDURE sp_get_Farmacia
+	@CedJuridica BIGINT
+AS
+BEGIN
+	SELECT Nombre,CedJuridica, Ubicacion,Telefono,Correo,Horario,TotalRecaudado
+	FROM Farmacia
+	WHERE CedJuridica=@CedJuridica
+END
+GO
+/* Obtener full info de pedido por Código de pedido */
+CREATE PROCEDURE sp_get_Pedido
+	@CodigoDePedido VARCHAR(MAX)
+AS
+BEGIN
+	SELECT f.Nombre,f.CedJuridica,p.Fecha,p.Estado,p.Monto,p.CodigoPedido,p.Tipo,c.Nombre,c.Apellido1,c.Apellido2,c.Cedula 
+	AS InfoPedido
+	FROM Pedido p 
+	INNER JOIN Farmacia f ON p.IdFarmacia=f.Id
+	INNER JOIN Cliente c ON p.IdCliente=c.Id
+	WHERE p.CodigoPedido=@CodigoDePedido
+END
+GO
+/* Obtener info de empleado por correo */
+CREATE PROCEDURE sp_get_Empleado
+	@Correo VARCHAR(MAX)
+AS
+BEGIN
+	SELECT Nombre,Apellido1,Apellido2,Tipo,Estado,Correo 
+	AS Empleado 
+	FROM Empleado
+	WHERE Correo=@Correo
+END
+GO
+/* Obtener Total General */
+CREATE PROCEDURE sp_get_MontoGeneral
+AS
+BEGIN
+	SELECT SUM(TotalRecaudado) 
+	AS TotalGeneral
+	FROM Farmacia
+END
+GO
+/* Obtener total de clientes */
+CREATE PROCEDURE sp_get_TotalClientes
+AS
+BEGIN
+	SELECT COUNT(Id) 
+	AS CantidadDeClientes
+	FROM Cliente
+END
+GO
 
 /**************** UPDATE *****************/
+
+/* Cambiar monto total recaudado por sucursal */
 CREATE PROCEDURE sp_update_MontoFarmacia
-	@IdFarmacia INT,
+	@CedJuridica BIGINT,
 	@Monto MONEY
 AS
 BEGIN
+	DECLARE @IdFarmacia INT;
+	SET @IdFarmacia = 0;
+	SELECT @IdFarmacia=Id FROM Farmacia WHERE CedJuridica=@CedJuridica
 	UPDATE Farmacia SET TotalRecaudado=@Monto WHERE Id=@IdFarmacia
 END
 GO
+/* Cambiar estado del pedido */
 CREATE PROCEDURE sp_update_EstadoPedido
 	@IdPedido INT,
 	@Estado INT
 AS
 BEGIN
 	UPDATE Pedido SET Estado=@Estado WHERE Id=@IdPedido
+END
+GO
+/* Actualizar stock */
+CREATE PROCEDURE sp_update_FarmaciaXMedicamento_sin_fk
+	@CedJuridica BIGINT,
+	@CodigoDeMedicamento VARCHAR(MAX),
+	@Stock INT
+AS
+BEGIN
+	DECLARE @IdFarmacia INT;
+	SET @IdFarmacia = 0;
+	SELECT @IdFarmacia=Id FROM Farmacia WHERE CedJuridica=@CedJuridica
+	DECLARE @IdMedicamento INT;
+	SET @IdMedicamento = 0;
+	SELECT @IdMedicamento=Id FROM Medicamento WHERE CodigoDeMedicamento=@CodigoDeMedicamento
+	UPDATE FarmaciaXMedicamento SET Stock=@Stock WHERE IdFarmacia=@IdFarmacia AND IdMedicamento=@IdMedicamento
+END
+GO
+/* Actualizar cantidad de X medicamento en el pedido*/
+CREATE PROCEDURE sp_update_CantidadDeMedicamentoEnPedido
+	@CodigoDePedido VARCHAR(MAX),
+	@CodigoDeMedicamento VARCHAR(MAX),
+	@Cantidad INT
+AS
+BEGIN
+	DECLARE @IdPedido INT;
+	SET @IdPedido = 0;
+	SELECT @IdPedido=Id FROM Pedido WHERE CodigoPedido=@CodigoDePedido
+	DECLARE @IdMedicamento INT;
+	SET @IdMedicamento = 0;
+	SELECT @IdMedicamento=Id FROM Medicamento WHERE CodigoDeMedicamento=@CodigoDeMedicamento
+	UPDATE PedidoXMedicamento SET Cantidad=@Cantidad WHERE IdPedido=@IdPedido AND IdMedicamento=@IdMedicamento
+END
+GO
+
+/**************** DELETE *****************/
+/* Eliminar medicamento de pedido */
+CREATE PROCEDURE sp_delete_PedidoXMedicamento
+	@CodigoDePedido VARCHAR(MAX),
+	@CodigoDeMedicamento VARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @IdPedido INT;
+	SET @IdPedido = 0;
+	SELECT @IdPedido=Id FROM Pedido WHERE CodigoPedido=@CodigoDePedido
+	DECLARE @IdMedicamento INT;
+	SET @IdMedicamento = 0;
+	SELECT @IdMedicamento=Id FROM Medicamento WHERE CodigoDeMedicamento=@CodigoDeMedicamento
+	DELETE FROM PedidoXMedicamento WHERE IdMedicamento=@IdMedicamento AND IdPedido=@IdPedido
 END
 GO
